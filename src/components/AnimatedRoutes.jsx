b2e3d8c482dac887dc1982aaedeafaa090ab188e
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-07-19 11:34:45 -0500

    navbar

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,8 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Auth from "./Auth/Auth";
 import Home from "../pages/Home/Home";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
 
 const AnimatedRoutes = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Auth />} />
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlinePoweroff } from "react-icons/ai";
+import { signOut } from "firebase/auth";
+import { auth } from "../../config/firebase";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+      console.log("Signed out successfully");
+    } catch (error) {
+      return (
+        <h1>Error signing you out try again later</h1>
+      );
+    }
+  };
+
+  return (
+    <nav>
+      <div className="container nav__container">
+        <Link to="/home">
+          DGram
+        </Link>
+        <button onClick={logout}>
+          <AiOutlinePoweroff />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
